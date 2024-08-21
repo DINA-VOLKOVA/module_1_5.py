2a2abd5410a98d280c049ed38bd7a35825af4e30
DINA-VOLKOVA <homeyhill@mail.ru>
2024-08-21 22:57:55 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,8 @@
+
+
+immutable_var = 1, 2, 3, 'a', 'b', 'c'
+print(immutable_var)
+mutable_list = ([1, 2, 3, 'a', 'b', 'c'], 0)
+print(mutable_list)
+mutable_list [0][0] = 'List'
+print(mutable_list)
